4ff53e2ea24f0588e54247e470f241ed74d6d2e2
Andrew Thompson <andrewthompson@Andrews-M1-16-MBP-Pro.local>
2023-10-24 07:01:36 -0700

    done with tailwind, redux time

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,10 +1,13 @@
-import { Link } from 'react-router-dom';
-import SearchOrder from '../features/order/SearchOrder';
-import Username from '../features/user/Username';
+import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/user/Username";
 
 function Header() {
   return (
-    <header className="border-b border-stone-300 bg-yellow-500 px-4 py-3 uppercase">
+    <header
+      className="font-pizza flex items-center justify-between border-b
+    border-stone-300 bg-yellow-400 px-4 py-3 uppercase sm:px-6"
+    >
       <Link to="/" className="tracking-widest">
         Fast React Pizza Co.
       </Link>
